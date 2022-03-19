3fcaf68431cb4d540c49f127079836b7f95f5237
Muhammad Sohaib Khan <sohaibkhan.sk100@gmail.com>
2022-03-19 15:16:10 +0500

    done deal

---
+++ dataapp/android/app/src/main/kotlin/com/example/dataapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dataapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dataapp/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
